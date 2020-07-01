bae5d3ac17db0b7f3bbd291851ece7f02adf5b2e
AlexeyStitcg <ljrevtyn12!>
2020-07-01 18:42:48 +0500

    Kanban board + use Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+    const addTask = () => {
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+        <div className="container">
+            <h1>Kanban</h1>
+
+
+            <button className="btn btn-primary">Create Task</button>
+
+            <form>
+                <div className="form-group">
+
+                    <label htmlFor="exampleInputEmail1">Task</label>
+                    <input type="text" className="form-control"/>
+                    </div>
+
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+
+
+            <div className="row">
+                <div className="col-sm">
+                    To do
+                </div>
+
+                <div className="col-sm">
+                    In Progress
+                </div>
+
+                <div className="col-sm">
+                    Review
+                </div>
+
+                <div className="col-sm">
+                    Done
+                </div>
+
+            </div>
+        </div>
+
+
     </div>
   );
 }
